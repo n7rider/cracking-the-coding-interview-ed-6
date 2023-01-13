63404e41bdc67ee64795d0cdf17ddae87bb1bb14
n7rider <sjoedanny@gmail.com>
2023-01-12 23:00:02 -0500

    Add question

--- cracking-the-coding-interview-ed-6/src/main/java/n7rider/chapter1/Question1_1.java
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/chapter1/Question1_1.java
@@ -1,4 +1,10 @@
 package n7rider.chapter1;
 
+/**
+ * 1.1
+ * Is Unique: Implement an algorithm to determine if a string has all unique characters. What if you
+ * cannot use additional data structures?
+ * Hints: #44, #7 7 7, #732
+ */
 public class Question1_1 {
 }
