b45aef9d5febd7031153bb46a4bc480d9dcc3945
Joe <sjoedanny@gmail.com>
2023-01-20 09:41:06 -0500

    Add solution for 1.1

---
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/chapter1/Question1_2.java
@@ -0,0 +1,11 @@
+package n7rider.chapter1;
+
+/**
+ * 1.2 Check Permutation: Given two strings, write a method to decide if one is a permutation of the other.
+ * Hints: #1, #84, #122, #131
+ */
+public class Question1_2 {
+    public static void main(String[] args) {
+
+    }
+}
