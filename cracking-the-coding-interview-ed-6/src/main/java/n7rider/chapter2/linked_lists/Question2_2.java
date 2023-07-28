e901f72280c9863f31ed21afd2e3b556aad7ca19
n7rider <sjoedanny@gmail.com>
2023-07-28 15:34:39 -0400

    Add redo for chapter 1, solution for 2.1

---
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/chapter2/linked_lists/Question2_2.java
@@ -0,0 +1,14 @@
+package n7rider.chapter2.linked_lists;
+
+/**
+ * 2.2
+ * Return Kth to Last: Implement an algorithm to find the kth to last element of a singly linked list.
+ * Hints:#8, #25, #41, #67, #126
+ * ---
+ *  * Post-run observations:
+ *
+ *  * After comparing with solution:
+ *
+ */
+public class Question2_2 {
+}
---
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/chapter2/linked_lists/Question2_3.java
@@ -0,0 +1,19 @@
+package n7rider.chapter2.linked_lists;
+
+/**
+ * 2.3
+ * Delete Middle Node: Implement an algorithm to delete a node in the middle (i.e., any node but
+ * the first and last node, not necessarily the exact middle) of a singly linked list, given only access to
+ * that node.
+ * EXAMPLE
+ * lnput:the node c from the linked lista->b->c->d->e->f
+ * Result: nothing is returned, but the new linked list looks like a->b->d->e->f
+ * Hints:#72
+ * ---
+ *  * Post-run observations:
+ *
+ *  * After comparing with solution:
+ * 
+ */
+public class Question2_3 {
+}
