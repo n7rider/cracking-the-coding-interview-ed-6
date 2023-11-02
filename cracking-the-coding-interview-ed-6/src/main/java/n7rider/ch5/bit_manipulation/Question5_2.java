257af3e1faa7965261a699ffec06ab2dd3ccff93
n7rider <sjoedanny@gmail.com>
2023-11-02 18:24:37 -0400

    Add questions for 5.1-5.8

---
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/ch5/bit_manipulation/Question5_2.java
@@ -0,0 +1,11 @@
+package n7rider.ch5.bit_manipulation;
+
+/**
+ * 5.2
+ * Binary to String: Given a real number between O and 1 (e.g., 0.72) that is passed in as a double, print
+ * the binary representation. If the number cannot be represented accurately in binary with at most 32
+ * characters, print "ERROR:'
+ * Hints: #743, #767, #7 73, #269, #297
+ */
+public class Question5_2 {
+}
---
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/ch5/bit_manipulation/Question5_8.java
@@ -0,0 +1,14 @@
+package n7rider.ch5.bit_manipulation;
+
+/**
+ * 5.8
+ * Draw Line: A monochrome screen is stored as a single array of bytes, allowing eight consecutive
+ * pixels to be stored in one byte. The screen has width w, where w is divisible by 8 (that is, no byte will
+ * be split across rows). The height of the screen, of course, can be derived from the length of the array
+ * and the width. Implement a function that draws a horizontal line from ( xl, y) to ( x2, y).
+ * The method signature should look something like:
+ * drawline(byte[] screen, int width, int xl, int x2, int y)
+ * Hints: #366, #387, #384, #397
+ */
+public class Question5_8 {
+}
