4ad3ae3247efc128c90696ad1a891681d3fb801d
n7rider <sjoedanny@gmail.com>
2023-12-11 20:44:43 -0500

    Add questions for chapter 6

---
+++ cracking-the-coding-interview-ed-6/src/main/java/n7rider/ch6/math_and_logic/Question6_9.java
@@ -0,0 +1,12 @@
+package n7rider.ch6.math_and_logic;
+
+/**
+ * 6.9
+ * 100 Lockers: There are 100 closed lockers in a hallway. A man begins by opening all 100 lockers.
+ * Next, he closes every second locker. Then, on his third pass, he toggles every third locker (closes it if
+ * it is open or opens it if it is closed). This process continues for 100 passes, such that on each pass i,
+ * the man toggles every ith locker. After his 100th pass in the hallway, in which he toggles only locker
+ * #100, how many lockers are open?
+ */
+public class Question6_9 {
+}
